b5bbd334106a7f750df44c086f6bb77562227aef
xzxHomeAssistant <150352280+xzxHomeAssistant@users.noreply.github.com>
2024-03-01 23:09:44 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -8,6 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git amlogic https://github.com/ophub/luci-app-amlogic' feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
